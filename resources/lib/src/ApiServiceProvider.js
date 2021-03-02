711d90728799890baa233027a77028fb7ddd924b
Ryan Thompson <service@aiwebsystems.com>
2021-03-02 13:51:33 -0600

    CS

--- resources/lib/src/ApiServiceProvider.js
+++ resources/lib/src/ApiServiceProvider.js
@@ -3,6 +3,10 @@ import { ServiceProvider } from '@streams/core';
 export class ApiServiceProvider extends ServiceProvider {
 
     async register() {
-        //alert('API');
+        //
+    }
+    
+    async boot() {
+        //
     }
 }
