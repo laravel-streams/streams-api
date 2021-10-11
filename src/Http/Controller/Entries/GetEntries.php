7702a367d120cbf07201958dbdf670e3b0fe0d4c
Robin Radic <rradic@hotmail.com>
2021-10-11 06:05:08 +0200

    Provide paginated and non-paginated responses

--- src/Http/Controller/Entries/GetEntries.php
+++ src/Http/Controller/Entries/GetEntries.php
@@ -3,9 +3,9 @@
 namespace Streams\Api\Http\Controller\Entries;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\URL;
 use Streams\Core\Support\Facades\Streams;
 
 class GetEntries extends Controller
@@ -19,31 +19,47 @@ class GetEntries extends Controller
      */
     public function __invoke($stream)
     {
-        $paginator = Streams::entries($stream)
-            ->loadParameters(Request::json('query', []))
-            ->paginate([
-                'per_page' => Request::get('per_page', 100),
-                'page' => Request::get('page', 1),
-            ]);
+        $criteria = Streams::entries($stream)->loadParameters(Request::json('query', []));
 
+        if (Request::has('paginate')) {
+            $paginator = $criteria->paginate([
+                'per_page' => Request::get('per_page', 100),
+                'page'     => Request::get('page', 1),
+            ]);
+            return Response::json([
+                'meta'  => [
+                    'total'        => $paginator->total(),
+                    'per_page'     => $paginator->perPage(),
+                    'last_page'    => $paginator->lastPage(),
+                    'current_page' => $paginator->currentPage(),
+                    'parameters'   => Request::route()->parameters(),
+                    'payload'      => Request::json(),
+                ],
+                'links' => [
+                    'self'          => URL::full(),
+                    'first_page'    => $paginator->url(1),
+                    'next_page'     => $paginator->nextPageUrl(),
+                    'previous_page' => $paginator->previousPageUrl(),
+                    'streams'       => URL::route('streams.api.streams.index'),
+                    'stream'        => URL::route('streams.api.streams.show', [ 'stream' => $stream ]),
+                ],
+                'data'  => $paginator->getCollection(),
+            ]);
+        }
+
+        $entries = $criteria->get();
         return Response::json([
-            'meta' => [
-                'total' => $paginator->total(),
-                'per_page' => $paginator->perPage(),
-                'last_page' => $paginator->lastPage(),
-                'current_page' => $paginator->currentPage(),
+            'meta'  => [
+                'total'      => $entries->count(),
                 'parameters' => Request::route()->parameters(),
-                'payload' => Request::json(),
+                'payload'    => Request::json(),
             ],
             'links' => [
-                'self' => URL::full(),
-                'first_page' => $paginator->url(1),
-                'next_page' => $paginator->nextPageUrl(),
-                'previous_page' => $paginator->previousPageUrl(),
+                'self'    => URL::full(),
                 'streams' => URL::route('streams.api.streams.index'),
-                'stream' => URL::route('streams.api.streams.show', ['stream' => $stream]),
+                'stream'  => URL::route('streams.api.streams.show', [ 'stream' => $stream ]),
             ],
-            'data' => $paginator->getCollection(),
+            'data'  => $entries->toArray(),
         ]);
     }
 }
