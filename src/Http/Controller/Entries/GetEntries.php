e7d2c19b37b7c9ee9236fb6321fe13445ad1457c
Ryan Thompson <service@aiwebsystems.com>
2021-11-15 01:24:45 -0600

    Always paginate. Protect inward - not in the controller

--- src/Http/Controller/Entries/GetEntries.php
+++ src/Http/Controller/Entries/GetEntries.php
@@ -2,76 +2,53 @@
 
 namespace Streams\Api\Http\Controller\Entries;
 
-use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\URL;
-use Streams\Api\Http\Controller\ApiController;
-use Streams\Core\Criteria\Criteria;
 use Streams\Core\Support\Facades\Streams;
+use Streams\Api\Http\Controller\ApiController;
 
 class GetEntries extends ApiController
 {
 
-    /**
-     * Return all entries for the stream.
-     *
-     * @param $stream
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
     public function __invoke($stream)
     {
 
         $headers = [];
 
         // atm doing this in steps for xdebug
-        $payload = collect(Request::query('parameters',[]));
-        $payload = $payload->filter(function ($value, $key) {
+        $parameters = collect(Request::query('parameters', []));
+        $parameters = $parameters->filter(function ($value, $key) {
             // all statements from frontend streams api are in this array with a numeric key
             // filtering out stuff like XDEBUG_SESSION=PHPSTORM, which shouldn't be load as parameter
             return is_numeric($key);
         });
-        $payload = $payload->map(function ($value) {
-            // Mapping to see if the $value array keys are methods of Criteria.
-            // Otherwise, they should not be loaded as parameter, avoid hacks and bugs
-            // we filter them out of the query
-            $keys = array_filter(array_keys($value), static function ($key) {
-                return method_exists(Criteria::class, $key);
-            });
-            return Arr::only($value, $keys);
-        });
-        $payload = $payload->toArray();
-        $criteria = Streams::entries($stream)->loadParameters($payload);
+        $parameters = $parameters->toArray();
+        $criteria = Streams::entries($stream)->loadParameters($parameters);
+
+        $results = $criteria->paginate([
+            'per_page' => Request::get('per_page', 100),
+            'page'     => Request::get('page', 1),
+        ]);
 
         $meta = [
-            'parameters' => Request::route()->parameters(),
-            'payload'    => $payload,
+            'total' => $results->total(),
+            'per_page' => $results->perPage(),
+            'last_page' => $results->lastPage(),
+            'current_page' => $results->currentPage(),
+            'route_parameters' => Request::route()->parameters(),
+            'parameters' => $parameters,
         ];
 
         $links = [
+            'first_page' => $results->url(1),
+            'next_page' => $results->nextPageUrl(),
+            'previous_page' => $results->previousPageUrl(),
+
             'self'    => URL::full(),
             'streams' => URL::route('streams.api.streams.index'),
-            'stream'  => URL::route('streams.api.streams.show', [ 'stream' => $stream ]),
+            'stream'  => URL::route('streams.api.streams.show', ['stream' => $stream]),
         ];
-
-        if (Request::query('paginate')) {
-            $results = $criteria->paginate([
-                'per_page' => Request::get('per_page', 100),
-                'page'     => Request::get('page', 1),
-            ]);
-
-            $meta[ 'total' ]        = $results->total();
-            $meta[ 'per_page' ]     = $results->perPage();
-            $meta[ 'last_page' ]    = $results->lastPage();
-            $meta[ 'current_page' ] = $results->currentPage();
-
-            $links[ 'first_page' ]    = $results->url(1);
-            $links[ 'next_page' ]     = $results->nextPageUrl();
-            $links[ 'previous_page' ] = $results->previousPageUrl();
-        } else {
-            $results = $criteria->get();
-            $meta[ 'total' ] = $results->count();
-        }
 
         return Response::json([
             'meta'  => $meta,
