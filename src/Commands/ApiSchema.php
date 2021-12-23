2543876b991473896bc5860f22938cfd92b7e44e
Robin Radic <rradic@hotmail.com>
2021-12-23 03:45:49 +0100

    Added api schema validation

--- src/Commands/ApiSchema.php
+++ src/Commands/ApiSchema.php
@@ -2,7 +2,9 @@
 
 namespace Streams\Api\Commands;
 
+use GoldSpecDigital\ObjectOrientedOAS\Exceptions\ValidationException;
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 use Streams\Api\Facades\Api;
 use Symfony\Component\Yaml\Yaml;
@@ -13,10 +15,25 @@ class ApiSchema extends Command
 
     public function handle()
     {
-        $yaml = Yaml::dump(Api::schema()->create()->toArray(), 100);
-
-        File::put($path = 'bootstrap/cache/api.yaml', $yaml);
-
-        $this->info('Dumped YAML to '.base_path($path));
+        /** @var \GoldSpecDigital\ObjectOrientedOAS\OpenApi $schema */
+        $schema = Api::schema()->create();
+        try {
+            $schema->validate();
+            $yaml = Yaml::dump($schema->toArray(), 100);
+            File::put($path = 'bootstrap/cache/api.yaml', $yaml);
+            $this->info('Dumped YAML to ' . base_path($path));
+        }
+        catch (ValidationException $e) {
+            foreach($e->getErrors() as $error){
+                $this->error(
+                    Arr::pull($error,'message') . ':'
+                );
+                $this->line(collect($error)->transform(fn($v,$k) => "$k: $v")->implode("\n"));
+                $this->getOutput()->newLine();
+            }
+        }
+        catch (\Throwable $e) {
+            return $this->error($e->getMessage());
+        }
     }
 }
