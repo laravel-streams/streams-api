c7fffaa0cb577a1fa6314ff87acd72a7a7da1307
Ryan Thompson <ryan@pyrocms.com>
2024-07-30 12:55:58 -0500

    API stuff

--- src/ApiManager.php
+++ src/ApiManager.php
@@ -2,10 +2,11 @@
 
 namespace Streams\Api;
 
+use Illuminate\Support\Facades\Route;
+
 class ApiManager
 {
     protected array $interfaces = [];
-    protected array $booted = [];
 
     public function interface(ApiInterface $interface): void
     {
@@ -17,6 +18,83 @@ class ApiManager
     public function getInterfaces()
     {
         return $this->interfaces;
-        // return $this->evalulate($this->interfaces);
+    }
+
+    public function routeStreams()
+    {
+        /*
+        * Route Streams API endpoints.
+        */
+        Route::get('streams', [
+            'uses' => \Streams\Api\Http\Controller\Streams\GetStreams::class,
+            'as' => 'streams.api.streams.list',
+        ]);
+        Route::post('streams', [
+            'uses' => \Streams\Api\Http\Controller\Streams\CreateStream::class,
+            'as' => 'streams.api.streams.create',
+        ]);
+        Route::get('streams/{stream}', [
+            'uses' => \Streams\Api\Http\Controller\Streams\ShowStream::class,
+            'as' => 'streams.api.streams.show',
+        ]);
+        Route::put('streams/{stream}', [
+            'uses' => \Streams\Api\Http\Controller\Streams\UpdateStream::class,
+            'as' => 'streams.api.streams.update',
+        ]);
+        Route::patch('streams/{stream}', [
+            'uses' => \Streams\Api\Http\Controller\Streams\PatchStream::class,
+            'as' => 'streams.api.streams.patch',
+        ]);
+        Route::delete('streams/{stream}', [
+            'uses' => \Streams\Api\Http\Controller\Streams\DeleteStream::class,
+            'as' => 'streams.api.streams.delete',
+        ]);
+    }
+
+    public function routeEntries()
+    {
+        /*
+        * Route entries API endpoints.
+        */
+        Route::get('streams/{stream}/entries', [
+            'uses' => \Streams\Api\Http\Controller\Entries\GetEntries::class,
+            'as' => 'streams.api.entries.list',
+        ]);
+        Route::post('streams/{stream}/entries', [
+            'uses' => \Streams\Api\Http\Controller\Entries\CreateEntry::class,
+            'as' => 'streams.api.entries.create',
+        ]);
+        Route::get('streams/{stream}/entries/{entry}', [
+            'uses' => \Streams\Api\Http\Controller\Entries\ShowEntry::class,
+            'as' => 'streams.api.entries.show',
+            'where' => [
+                'entry' => '(.*)',
+            ],
+        ]);
+        Route::put('streams/{stream}/entries/{entry}', [
+            'uses' => \Streams\Api\Http\Controller\Entries\UpdateEntry::class,
+            'as' => 'streams.api.entries.update',
+            'where' => [
+                'entry' => '(.*)',
+            ],
+        ]);
+        Route::patch('streams/{stream}/entries/{entry}', [
+            'uses' => \Streams\Api\Http\Controller\Entries\PatchEntry::class,
+            'as' => 'streams.api.entries.patch',
+            'where' => [
+                'entry' => '(.*)',
+            ],
+        ]);
+        Route::delete('streams/{stream}/entries/{entry}', [
+            'uses' => \Streams\Api\Http\Controller\Entries\DeleteEntry::class,
+            'as' => 'streams.api.entries.delete',
+            'where' => [
+                'entry' => '(.*)',
+            ],
+        ]);
+        Route::post('streams/{stream}/query', [
+            'uses' => \Streams\Api\Http\Controller\Entries\QueryEntries::class,
+            'as' => 'streams.api.entries.query',
+        ]);
     }
 }
--- src/ApiServiceProvider.php
+++ src/ApiServiceProvider.php
@@ -5,22 +5,7 @@ namespace Streams\Api;
 use Streams\Api\Support\Facades\API;
 use Streams\Core\Support\Integrator;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
-use Streams\Api\Http\Controller\Entries\ShowEntry;
-use Streams\Api\Http\Controller\Entries\GetEntries;
-use Streams\Api\Http\Controller\Entries\PatchEntry;
-use Streams\Api\Http\Controller\Streams\GetStreams;
-use Streams\Api\Http\Controller\Streams\ShowStream;
-use Streams\Api\Http\Controller\Entries\CreateEntry;
-use Streams\Api\Http\Controller\Entries\DeleteEntry;
-use Streams\Api\Http\Controller\Entries\UpdateEntry;
-use Streams\Api\Http\Controller\Streams\PatchStream;
-use Streams\Api\Http\Controller\Entries\QueryEntries;
-use Streams\Api\Http\Controller\Streams\CreateStream;
-use Streams\Api\Http\Controller\Streams\DeleteStream;
-use Streams\Api\Http\Controller\Streams\UpdateStream;
-
 class ApiServiceProvider extends ServiceProvider
 {
     public function boot()
@@ -53,15 +38,15 @@ class ApiServiceProvider extends ServiceProvider
 
                         foreach ([null] as $domain) {
 
-                            if ($routes = $interface->getRoutes()) {
-                                $routes($interface);
-                            }
-
                             Route::domain($domain)
                                 ->middleware($interface->getMiddleware())
                                 ->name($id . '.')
                                 ->prefix($path)
                                 ->group(function () use ($interface) {
+
+                                    if ($routes = $interface->getRoutes()) {
+                                        $routes($interface);
+                                    }
 
                                     foreach ($interface->getEndpoints() as $route => $endpoint) {
                                         // $endpoint::routes($interface);
@@ -104,85 +89,5 @@ class ApiServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../resources/config/api.php' => config_path('streams/api.php'),
         ], 'config');
-    }
-
-    protected function registerRoutes(): void
-    {
-        Route::prefix(Config::get('streams.api.prefix', 'api'))
-            ->middleware(Config::get('streams.api.middleware', 'api'))
-            ->group(function () {
-
-                /*
-                 * Route Streams API endpoints.
-                 */
-                Route::get('streams', [
-                    'uses' => GetStreams::class,
-                    'as'   => 'streams.api.streams.list',
-                ]);
-                Route::post('streams', [
-                    'uses' => CreateStream::class,
-                    'as'   => 'streams.api.streams.create',
-                ]);
-                Route::get('streams/{stream}', [
-                    'uses' => ShowStream::class,
-                    'as'   => 'streams.api.streams.show',
-                ]);
-                Route::put('streams/{stream}', [
-                    'uses' => UpdateStream::class,
-                    'as'   => 'streams.api.streams.update',
-                ]);
-                Route::patch('streams/{stream}', [
-                    'uses' => PatchStream::class,
-                    'as'   => 'streams.api.streams.patch',
-                ]);
-                Route::delete('streams/{stream}', [
-                    'uses' => DeleteStream::class,
-                    'as'   => 'streams.api.streams.delete',
-                ]);
-
-                /*
-                 * Route entries API endpoints.
-                 */
-                Route::get('streams/{stream}/entries', [
-                    'uses'  => GetEntries::class,
-                    'as'    => 'streams.api.entries.list',
-                ]);
-                Route::post('streams/{stream}/entries', [
-                    'uses' => CreateEntry::class,
-                    'as'   => 'streams.api.entries.create',
-                ]);
-                Route::get('streams/{stream}/entries/{entry}', [
-                    'uses'  => ShowEntry::class,
-                    'as'    => 'streams.api.entries.show',
-                    'where' => [
-                        'entry' => '(.*)',
-                    ],
-                ]);
-                Route::put('streams/{stream}/entries/{entry}', [
-                    'uses' => UpdateEntry::class,
-                    'as'   => 'streams.api.entries.update',
-                    'where' => [
-                        'entry' => '(.*)',
-                    ],
-                ]);
-                Route::patch('streams/{stream}/entries/{entry}', [
-                    'uses' => PatchEntry::class,
-                    'as'   => 'streams.api.entries.patch',
-                    'where' => [
-                        'entry' => '(.*)',
-                    ],
-                ]);
-                Route::delete('streams/{stream}/entries/{entry}', [
-                    'uses' => DeleteEntry::class,
-                    'as'   => 'streams.api.entries.delete',
-                    'where' => [
-                        'entry' => '(.*)',
-                    ],
-                ]);
-                Route::post('streams/{stream}/query', [
-                    'uses'  => QueryEntries::class,
-                    'as'    => 'streams.api.entries.query',
-                ]);
-            });
     }
 }
